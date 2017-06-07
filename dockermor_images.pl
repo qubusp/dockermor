1c4e3887bc360144409e7378154fd933f635a3b1
qubusp <jakob.r.jakov@gmail.com>
2017-06-07 14:24:51 +0200

    Update dockermor_images.pl

--- dockermor_images.pl
+++ dockermor_images.pl
@@ -1,32 +1,62 @@
 #!/usr/bin/perl
 #
 # @File docker mor.pl
-# @Author qubsup
+# @Author qubsup - with the help of Dr. Ondrej Guth
 # @Created May 30, 2017 10:27:01 AM
 #
-
-
-my $imd = qx/docker images  --format "{{.ID}}: {{.CreatedSince}}" | grep -i days/;
-
-
+use Getopt::Long;
+my $ha;
+my $da;
+my $mo;
+my $we;
+GetOptions('ha=i' => \$ha,
+           'da=i' => \$da,
+           'mo=i' => \$mo,
+           'we=i' => \$we
+) or die "Please use -h to set hours or -d to set days old image\n";
+if ($da >= 0 ){
+my $imd = qx/docker images  --format "{{.ID}}: {{.CreatedSince}} {{.Repository}}" | grep -i days/;
 foreach my $d (split(/\n/, $imd)){
 	my @words = split(' ', $d);
    	my $daily = $words[1];
-      	if ($daily >= 1){
-          system("docker rmi -f $words[0]") == 0 or die('docker rmi failed');
-          print "docker image $words[0] deleted";
+      	if ($daily >= $da){
+          system("docker rmi -f $words[3]") == 0 or die('docker rmi failed');
+          print "docker image $words[3] deleted";
         }  
-}
+}};
 
-
-my $imh = qx/docker images  --format "{{.ID}}: {{.CreatedSince}}" | grep -i hours/;
+if ($ha >= 0 ) {
+my $imh = qx/docker images  --format "{{.ID}}: {{.CreatedSince}} {{.Repository}}" | grep -i hours/;
 
 foreach my $h (split(/\n/, $imh)){
 	my @words = split(' ', $h);
 	my $houry = $words[1];
-	if ($hourly >= 3){          
-          system("docker rmi -f $words[0]") == 0 or die('docker rmi failed');
-          print "docker image $words[0] deleted";
+	if ($hourly >= $ha){          
+          system("docker rmi -f $words[3]") == 0 or die('docker rmi failed');
+          print "docker image $words[3] deleted";
          }  
        
-};
+}};
+
+
+if ($mo >= 0 ){
+my $imm = qx/docker images  --format "{{.ID}}: {{.CreatedSince}} {{.Repository}}" | grep -i month/;
+foreach my $d (split(/\n/, $imm)){
+	my @words = split(' ', $d);
+   	my $monthly = $words[1];
+      	if ($monthly >= $mo){
+          system("docker rmi -f $words[3]") == 0 or die('docker rmi failed');
+          print "docker image $words[3] deleted";
+        }  
+}};
+
+if ($we >= 0 ){
+my $imw = qx/docker images  --format "{{.ID}}: {{.CreatedSince}} {{.Repository}}" | grep -i week/;
+foreach my $d (split(/\n/, $imw)){
+	my @words = split(' ', $d);
+   	my $weekly = $words[1];
+      	if ($weekly >= $we){
+          system("docker rmi  $words[3] -f") == 0 or die('docker rmi failed');
+          print "docker image $words[3] deleted";
+        }  
+}};
